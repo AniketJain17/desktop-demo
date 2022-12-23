58315cb381765ec55d78dc45d64c3c3d98b0c53f
Aniket Jain <63732758+AniketJain17@users.noreply.github.com>
2022-12-23 12:05:04 +0530

    Add files via upload

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+@file:Suppress("KotlinConstantConditions")
+
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -8,11 +10,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,34 +22,146 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+import javax.swing.JFileChooser
+//fun Route.uploadFile(){
+//	post("file"){
+//		val multipart = call.receiveMultipart()
+//		multipart.forEachPart { part ->
+//			when(part){
+//				is PartData.FormItem -> unit
+//				is PartData.FileItem -> {
+//					if(part.name == "image"){
+//						part.save("","")
+//					}
+//				}
+//				else -> unit
+//			}
+//		}
+//	}
+//}
+
+//fun openFile(view: View) {
+//	val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//	intent.addCategory(Intent.CATEGORY_OPENABLE)
+//	intent.type = "text/plain"
+//	startActivityForResult(intent, OPEN_REQUEST_CODE)
+//}
+//
+//fun saveFile(view: View) {
+//	val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//	intent.addCategory(Intent.CATEGORY_OPENABLE)
+//	intent.type = "text/plain"
+//
+//	startActivityForResult(intent, SAVE_REQUEST_CODE)
+//}
+
+// Using AWT From JAVA *************************************************************************************************
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//private fun FileDialog1(
+//	parent: Frame? = null,
+//	onCloseRequest: (result: String?) -> Unit
+//) = AwtWindow(
+//	create = {
+//		object : FileDialog(parent, "Choose a file", LOAD) {
+//			override fun setVisible(isselected: Boolean) {
+//				super.setVisible(isselected)
+//				if (isselected) {
+//					onCloseRequest(file)
+//					val path: String = FileDialog.
+//					val f = File(path)
+//					println(file)
+//					print(parent)
+//				}
+//			}
+//		}
+//	},
+//	dispose = FileDialog::dispose
+//
+//)
+//**********************************************************************************************************************
+
+fun selectFile(pathState: MutableState<String?>) {
+	JFileChooser().apply {
+		if (showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			pathState.value = selectedFile.path
+		}
+	}
+}
+
+//private fun moveFile(src: File, dest: File): Boolean {
+//	return src.renameTo(dest)
+//}
+//fun copyFile(src: File, dest: File) {
+//	FileInputStream(src).channel.use { sourceChannel ->
+//		FileOutputStream(dest).channel
+//			.use { destChannel -> destChannel.transferFrom(sourceChannel, 0, sourceChannel.size()) }
+//	}
+//}
 
 
+//private fun moveDir(src: Path, dest: Path): Boolean {
+//	if (src.toFile().isDirectory) {
+//		for (file in src.toFile().listFiles()) {
+//			moveDir(file.toPath(), dest.resolve(src.relativize(file.toPath())))
+//		}
+//	}
+//	return try {
+//		Files.move(src, dest, StandardCopyOption.REPLACE_EXISTING)
+//		true
+//	} catch (e: IOException) {
+//		false
+//	}
+//}
 
 @Composable
 @Preview
 fun App() {
 	Scaffold(
-		topBar = { TopAppBar(title = { Text("Desktop Menu", color = Color(0xFF3700B3)) }, backgroundColor = Color(0xff0f9d58)) },
+		topBar = { TopAppBar(title = { Text("Menu", color = Color(0xFF3700B3)) }, backgroundColor = Color(0xff0f9d58)) },
 		content = {
 			var mExpanded by remember { mutableStateOf(false) }
-			val mCities = listOf("Aadhar Card", "Pan Card", "Electricity Bill", "Ration Card", "Water Bill")
+			val mCities = listOf("Aadhaar Card", "Pan Card", "Electricity Bill", "Ration Card", "Water Bill")
 			var mSelectedText by remember { mutableStateOf("") }
 			var mTextFieldSize by remember { mutableStateOf(" ")}
 			var text by remember { mutableStateOf("") }
 			var password by remember { mutableStateOf("") }
+			val sourcePath = remember { mutableStateOf<String?>(null) }
+			val targetPath = remember { mutableStateOf<String?>(null) }
+
+			//File Choosing option  ************************************************************************************
+//			var isFileChooserOpen by remember { mutableStateOf(false) }
+//
+//			if (isFileChooserOpen) {
+//				FileDialog1(
+//					onCloseRequest = {
+//						isFileChooserOpen = false
+//					}
+//				)
+//			}
+			//**********************************************************************************************************
 
 			val icon = if (mExpanded)
 				Icons.Filled.KeyboardArrowUp
 			else
 				Icons.Filled.KeyboardArrowDown
 
+			//Full Name Text Area **************************************************************************************
 			Column(Modifier.padding(20.dp)) {
-				Text("Name", textAlign = TextAlign.Center)
+				Text("Full Name", textAlign = TextAlign.Center)
 				OutlinedTextField(
 					value = text,
 					onValueChange = { text = it },
 					label = { Text("Full Name") }
 				)
+
+				//Password Text Area ***********************************************************************************
 				Text("Enter Your Password", textAlign = TextAlign.Center)
 				OutlinedTextField(
 					value = password,
@@ -60,6 +170,8 @@ fun App() {
 					visualTransformation = PasswordVisualTransformation(),
 					keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
 				)
+
+				//Identification dropdown menu Text Area ***************************************************************
 				Text("Select Valid Identification ", textAlign = TextAlign.Center)
 				OutlinedTextField(
 					value = mSelectedText,
@@ -70,12 +182,9 @@ fun App() {
 						Icon(icon,"contentDescription", Modifier.clickable { mExpanded = !mExpanded })
 					}
 				)
-
-
 				DropdownMenu(
 					expanded = mExpanded,
 					onDismissRequest = { mExpanded = false },
-//					modifier = Modifier.width(with(LocalDensity.current){mTextFieldSize.width().toDp()})
 				) {
 					mCities.forEach { label ->
 						DropdownMenuItem(onClick = {
@@ -86,14 +195,59 @@ fun App() {
 						}
 					}
 				}
+				//******************************************************************************************************
+
+				//Choose File ******************************************************************************************
 				MaterialTheme {
 					Column(
 						horizontalAlignment = Alignment.CenterHorizontally,
 						modifier = Modifier.fillMaxWidth()) {
 						Button(onClick = {
-							text = "Choose File"
+//							isFileChooserOpen = true
+							selectFile(sourcePath)
+							println(sourcePath.value)
+
 						}) {
-							Text(text)
+							Text("Choose File")
+						}
+					}
+				}
+				//******************************************************************************************************
+
+				MaterialTheme {
+					Column(
+						horizontalAlignment = Alignment.CenterHorizontally,
+						modifier = Modifier.fillMaxWidth()) {
+						Button(onClick = {
+							//Creating New Folder using text
+							val folder = "C:\\Intern"
+							val fileName= text
+							val f = File(folder, fileName)
+							f.mkdir()
+							//Creating New Folder inside Name for identification
+							val folder1 = folder+"\\"+fileName
+							val fileName1= mSelectedText
+							val f1 = File(folder1, fileName1)
+							f1.mkdir()
+							val destinePath: String  =folder1+"\\"+fileName1
+							val destination:String = destinePath as String
+
+
+							val source= sourcePath.value as String
+							println(source)
+							println(destination)
+
+//							val success = moveDir(source.topath(),destinePath)
+							val sourcePath1 = Paths.get(source)
+							val targetPath1 = Paths.get(destination)
+//							Files.move(sourcePath1, targetPath1, StandardCopyOption.REPLACE_EXISTING)
+							File(source).let { sourceFile ->
+								sourceFile.copyTo(File(destination))
+								sourceFile.delete()
+							}
+
+						}) {
+							Text("SUBMIT")
 						}
 					}
 				}
@@ -102,6 +256,8 @@ fun App() {
 	)
 
 }
+
+
 
 fun main() = application {
 	Window(onCloseRequest = ::exitApplication) {
